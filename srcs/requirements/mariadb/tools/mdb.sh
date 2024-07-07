bf2b564106187ca2774937abc96db8fadacd5aee
Lyudmyla Andriychyk Fetsenets <lyandriy@c1r10s5.42madrid.com>
2024-07-07 16:22:54 +0200

    mariadb

--- srcs/requirements/mariadb/tools/mdb.sh
+++ srcs/requirements/mariadb/tools/mdb.sh
@@ -2,16 +2,12 @@
 
 service mariadb start
 
-echo "CREATE DATABASE IF NOT EXISTS ${DATABASE};
-CREATE USER IF NOT EXISTS ${USER_MDB}@'%' IDENTIFIED BY '${USER_PASS}';
-GRANT ALL PRIVILEGES ON ${DATABASE}.* TO ${USER_MDB}@'%';
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PASS}' ;
+echo "CREATE DATABASE IF NOT EXISTS $DATABASE;
+CREATE USER IF NOT EXISTS '$USER_MDB'@'%' IDENTIFIED BY '$USER_PASS';
+GRANT ALL PRIVILEGES ON $DATABASE.* TO $ROOT_PASS@'%';
+ALTER USER 'root'@'localhst' IDENTIFIED BY '$ROOT_PASS';
 FLUSH PRIVILEGES;" > /tmp/confmdb.sql
 
 mysql < /tmp/confmdb.sql
 
-kill $(cat /var/run/mysqld/mysqld.pid)
-
 rm /tmp/confmdb.sql
-
-exec mysqld
