031a4b592450e656833510da752d46dcc6b9e36a
Lyudmyla Andriychyk Fetsenets <lyandriy@c1r2s5.42madrid.com>
2024-07-08 17:48:41 +0200

    mdb

--- srcs/requirements/mariadb/tools/mdb.sh
+++ srcs/requirements/mariadb/tools/mdb.sh
@@ -2,14 +2,16 @@
 
 service mysql start
 
-echo "CREATE DATABASE IF NOT EXISTS $DATABASE;
+echo << EOF > /tmp/confmdb.sql
+CREATE DATABASE IF NOT EXISTS $DATABASE;
 CREATE USER IF NOT EXISTS '$USER_MDB'@'%' IDENTIFIED BY '$USER_PASS';
-GRANT ALL PRIVILEGES ON $DATABASE.* TO $ROOT_PASS@'%';
-ALTER USER 'root'@'localhst' IDENTIFIED BY '$ROOT_PASS';
-FLUSH PRIVILEGES;" > /tmp/confmdb.sql
+GRANT ALL PRIVILEGES ON $DATABASE.* TO '$USER_MDB'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$ROOT_PASS';
+FLUSH PRIVILEGES;
+EOF
 
 mysql < /tmp/confmdb.sql
 
-rm /tmp/confmdb.sql
+kill $(cat /var/run/mysqld/mysqld.pid)
 
-exec mysqld_safe
+exec /usr/bin/mysqld_safe
